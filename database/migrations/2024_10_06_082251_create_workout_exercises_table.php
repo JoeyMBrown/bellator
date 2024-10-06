e53dea4565024c6df3f63822c9afb9271aa08fd0
JoeyMBrown <joey.ma.brown@gmail.com>
2024-10-06 04:28:50 -0400

    Remove unnecessary indentation

--- database/migrations/2024_10_06_082251_create_workout_exercises_table.php
+++ database/migrations/2024_10_06_082251_create_workout_exercises_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('description');
             $table->unsignedBigInteger('exercise_type_id');
             $table->unsignedBigInteger('workout_id');
-
             $table->timestamps();
             $table->softDeletes();
 
