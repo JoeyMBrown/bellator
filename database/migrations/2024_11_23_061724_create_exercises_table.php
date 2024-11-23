55c6e6c303bbf5ff9833e7c1f643a4d6e67c6917
JoeyMBrown <joey.ma.brown@gmail.com>
2024-11-23 01:34:00 -0500

    Create exercise table

---
+++ database/migrations/2024_11_23_061724_create_exercises_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exercises', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', length: 60);
+            $table->string('description', length: 140)->nullable();
+            $table->foreignId('workout_id')->constrained('workouts');
+            $table->foreignId('exercise_type_id')->constrained('exercise_types');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exercises');
+    }
+};
