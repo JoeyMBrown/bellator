9791f7f40a6cc83b7122941a08433222b0cf18fb
JoeyMBrown <joey.ma.brown@gmail.com>
2024-11-22 19:08:22 -0500

    Create metric units table

---
+++ database/migrations/2024_11_23_000736_create_metric_units_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('metric_units', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', length: 60);
+            $table->string('description', length: 140);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('metric_units');
+    }
+};
