bbac86e8aaa8f50dd2da97df54df9a47c6680e1e
JoeyMBrown <joey.ma.brown@gmail.com>
2024-11-28 03:03:13 -0500

    Update workouts index page, abstract types to global type file

--- resources/js/Pages/Workouts/Index.tsx
+++ resources/js/Pages/Workouts/Index.tsx
@@ -1,22 +1,9 @@
 import BreadcrumbNav from '@/Components/Workouts/BreadcrumbNav';
-import WorkoutList from '@/Components/Workouts/WorkoutList';
+import WorkoutlistComponent  from '@/Components/Workouts/WorkoutList';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { Container } from '@mui/material';
-
-interface Workout { // TODO: Move types to index.d.ts or global.d.ts
-    id: number;
-    workout_date: string;
-    workout_type_id: null | number;
-    user_id: string;
-    created_at: null | string;
-    updated_at: null | string;
-    deleted_at: null | string;
-}
-
-interface WorkoutList {
-    workouts: Array<null | Workout>;
-}
+import { WorkoutList } from '@/types';
 
 const Create: React.FC<WorkoutList> = ({ workouts }) => {
 
@@ -36,7 +23,7 @@ const Create: React.FC<WorkoutList> = ({ workouts }) => {
                     currentPage='My Workouts'
                 />
                 
-                <WorkoutList workouts={workouts} />
+                <WorkoutlistComponent workouts={workouts} />
             </Container>
 
         </AuthenticatedLayout>
