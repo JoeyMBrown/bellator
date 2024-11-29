71fa846bfc882906eeb2ab75b34d67795092d143
JoeyMBrown <joey.ma.brown@gmail.com>
2024-11-29 09:23:32 -0500

    Redefine model as pivot model'

--- app/Models/WorkoutExercise.php
+++ app/Models/WorkoutExercise.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class WorkoutExercise extends Model
+class WorkoutExercise extends Pivot
 {
     use HasFactory, SoftDeletes;
 
