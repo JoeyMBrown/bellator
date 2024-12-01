65997330c2e647adf41938760d2c52bb19ad78d3
JoeyMBrown <joey.ma.brown@gmail.com>
2024-11-30 19:32:27 -0500

    Add repitions column to workout exercise logs table

--- database/migrations/2024_11_28_001319_create_workout_exercise_logs_table.php
+++ database/migrations/2024_11_28_001319_create_workout_exercise_logs_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('workout_exercise_logs', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('repitions')
+                ->comment('This can be used generally even when logging runs or swims (1 mile etc).');
             $table->decimal('exercise_metric', total: 16, places: 2);
-            $table->decimal('exercise_points', total: 8, places: 2);
+            $table->decimal('exercise_points', total: 8, places: 2); // TODO: Should this default to 0 or be set explicitly to 0 from controller on creation?
             $table->foreignId('workout_exercise_id')->constrained('workout_exercises');
             $table->foreignId('metric_unit_id')->constrained('metric_units');
             $table->timestamps();
