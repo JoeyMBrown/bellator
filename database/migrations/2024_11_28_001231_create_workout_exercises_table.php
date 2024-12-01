22d32ba13e708d4e735d7bdb7848597a2ae3ad71
JoeyMBrown <joey.ma.brown@gmail.com>
2024-12-01 12:12:22 -0500

    Update schema to enforce uniqueness of exercises tied to workouts

--- database/migrations/2024_11_28_001231_create_workout_exercises_table.php
+++ database/migrations/2024_11_28_001231_create_workout_exercises_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->foreignId('exercise_id')->constrained('exercises');
             $table->timestamps();
             $table->softDeletes();
+
+            $table->unique(['workout_id', 'exercise_id'])
+                ->comment('Enforces each workout can only contain a reference to 1 of each exercise.');
         });
     }
 
