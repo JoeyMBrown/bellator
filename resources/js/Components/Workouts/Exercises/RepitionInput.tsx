e771af7d4913d1b47ea6d154fafe89c9229ba514
JoeyMBrown <joey.ma.brown@gmail.com>
2024-12-02 13:47:28 -0500

    Create Repition Input component

---
+++ resources/js/Components/Workouts/Exercises/RepitionInput.tsx
@@ -0,0 +1,42 @@
+import {
+    TextField
+} from '@mui/material';
+import React from 'react';
+
+interface RepitionInputProps {
+    repitions: number | null;
+    setData: (arg0: any, arg2: any) => void;
+    setError: (arg0: any, arg2: any) => void;
+    error: string;
+};
+
+const RepitionInput: React.FC<RepitionInputProps> = ({ repitions, setData, setError, error }) => {
+
+    const validateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const regex = /^\d+$/;
+
+        setError('repitions', '');
+        setData('repitions', e.target.value);
+
+        if (e.target.value.length === 0) return setError('repitions', 'Repitions is a required field.');
+
+        if (!regex.test(e.target.value)) return setError('repitions', 'Repitions must be a number.');
+    }
+
+    return (
+            <TextField
+                id="repitions"
+                label="Repitions"
+                variant="outlined"
+                required
+                error={Boolean(error)}
+                helperText={error}
+                size='small'
+                type='text'
+                onChange={validateChange}
+                value={repitions}
+            />
+    );
+}
+
+export default RepitionInput;
