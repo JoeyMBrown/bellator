31ad8d6f9f4d2a08f1cd0ecfa61db6d4bc2aeb98
JoeyMBrown <joey.ma.brown@gmail.com>
2024-12-09 23:36:13 -0500

    Add typing and update workouts/exercises/show page

--- resources/js/Pages/Workouts/Exercises/Show.tsx
+++ resources/js/Pages/Workouts/Exercises/Show.tsx
@@ -1,42 +1,45 @@
 import BreadcrumbNav from '@/Components/Workouts/Exercises/BreadcrumbNav';
 import ExerciseLogForm from '@/Components/Workouts/Exercises/ExerciseLogForm';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { WorkoutExercise } from '@/types';
+import { MetricUnit, WorkoutExercise } from '@/types';
 import { Head } from '@inertiajs/react';
 import { Container } from '@mui/material';
 
-interface ShowProps {};
+interface ShowProps {
+    workoutExercise: WorkoutExercise;
+    metricUnitOptions: Array<MetricUnit | []>;
+}; // TODO: Define page props.
 
-export default function Show({workoutExercises}) {
+const Show: React.FC<ShowProps> = ({ workoutExercise, metricUnitOptions }) => {
 
     return (
         <AuthenticatedLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                    { workoutExercises.exercise?.name }
+                    { workoutExercise.exercise?.name }
                 </h2>
             }
         >
-            <Head title={workoutExercises.exercise?.name} />
+            <Head title={workoutExercise.exercise?.name} />
 
             <Container>
                 <BreadcrumbNav
-                    currentPage={workoutExercises.exercise?.name}
-                    workoutId={workoutExercises.workout.id}
-                    workoutDate={workoutExercises.workout.workout_date}
+                    currentPage={workoutExercise.exercise?.name}
+                    workoutId={workoutExercise?.workout?.id}
+                    workoutDate={workoutExercise?.workout?.workout_date}
                 />
 
-                <ExerciseLogForm />
-
-                { /** foreach exerciselog, generate a line item */}
-                { /** First input should be the repitions */}
-                { /** Second input should be the exercise_metric */}
-                { /** Third input should be the metric unit */}
-
-                {/** Exercise set logging form. */}
+                <ExerciseLogForm
+                    metricUnitOptions={metricUnitOptions}
+                    workoutExerciseLogs={workoutExercise.workout_exercise_logs}
+                    workoutExerciseId={workoutExercise.id}
+                    workoutId={workoutExercise?.workout?.id}
+                />
 
             </Container>
 
         </AuthenticatedLayout>
     );
 }
+
+export default Show;
